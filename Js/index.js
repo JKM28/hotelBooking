83294b5e481f24a05c9ffdf268a8619c83b1cd62
wawerks <rhowinlumales@gmail.com>
2024-10-17 00:14:11 +0800

    firstttttt

--- Js/index.js
+++ Js/index.js
@@ -6,49 +6,51 @@ $(document).ready(function() {
         $('#hotel-list-section').slideToggle();
     });
 
-  // Log when details are clicked
-$('.details').on('click', function(e) {
-    e.stopPropagation(); // Prevents the click from bubbling up
+    // Log when details are clicked
+    $('.details').on('click', function(e) {
+        e.stopPropagation(); // Prevents the click from bubbling up
 
-    // Get the hotel name from the closest anchor element
-    var hotelName = $(this).closest('.img-container').find('a').data('hotel'); 
-    console.log("Details clicked for: " + hotelName); // Log hotel name
+        // Get the hotel name from the closest anchor element
+        var hotelName = $(this).closest('.img-container').find('a').data('hotel'); 
+        console.log("Details clicked for: " + hotelName); // Log hotel name
 
-    // Set the hotel name in the modal and show it
-    $('#availabilityModal').modal('show');
-    $('#availabilityModal').data('hotel-name', hotelName).find('.modal-title').text('Check Availability for ' + hotelName);
-    $('#availabilityModal').find('#selected-hotel').val(hotelName);
-    
-    // Update hotel location
-    var hotelLocations = {
-        "Oasis Hotel": "Butuan City, Philippines",
-        "Inland Hotel": "Butuan City, Philippines",
-        "Watergate Hotel": "Butuan City, Philippines",
-        "Hotel 4": "Butuan City, Philippines",
-        "Hotel 5": "Butuan City, Philippines",
-        "Hotel 6": "Butuan City, Philippines",
-        "Hotel 7": "Butuan City, Philippines",
-        "Hotel 8": "Butuan City, Philippines",
-    };
+        // Set the hotel name in the modal and show it
+        $('#availabilityModal').modal('show');
+        $('#availabilityModal').data('hotel-name', hotelName).find('.modal-title').text('Check Availability for ' + hotelName);
+        $('#availabilityModal').find('#selected-hotel').val(hotelName);
+        
+        // Update hotel location
+        var hotelLocations = {
+            "Oasis Hotel": "Butuan City, Philippines",
+            "Inland Hotel": "Butuan City, Philippines",
+            "Watergate Hotel": "Butuan City, Philippines",
+            "Hotel 4": "Butuan City, Philippines",
+            "Hotel 5": "Butuan City, Philippines",
+            "Hotel 6": "Butuan City, Philippines",
+            "Hotel 7": "Butuan City, Philippines",
+            "Hotel 8": "Butuan City, Philippines",
+            "Hotel 9": "Butuan City, Philippines",
 
-    $('#availabilityModal').find('#hotel-location').val(hotelLocations[hotelName]);
+        };
 
-    // Update hotel price
-    var hotelPrices = {
-        "Oasis Hotel": "₱3,800",
-        "Inland Hotel": "₱4,000",
-        "Watergate Hotel": "₱3,800",
-        "Hotel 4": "₱2,500",
-        "Hotel 5": "₱5,200",
-        "Hotel 6": "₱6,000",
-        "Hotel 7": "₱4,800",
-        "Hotel 8": "₱3,200",
-    };
+        $('#availabilityModal').find('#hotel-location').val(hotelLocations[hotelName]);
 
-    $('#hotel-price').text(hotelPrices[hotelName]); // Set the price dynamically
-});
+        // Update hotel price
+        var hotelPrices = {
+            "Oasis Hotel": "₱3,800",
+            "Inland Hotel": "₱4,000",
+            "Watergate Hotel": "₱3,800",
+            "Hotel 4": "₱2,500",
+            "Hotel 5": "₱5,200",
+            "Hotel 6": "₱6,000",
+            "Hotel 7": "₱4,800",
+            "Hotel 8": "₱3,200",
+            "Hotel 9": "₱21,200",
 
-    
+        };
+
+        $('#hotel-price').text(hotelPrices[hotelName]); // Set the price dynamically
+    });
 
     // New functionality for See in Maps button
     $('#see-map-btn').on('click', function() {
@@ -164,4 +166,219 @@ $('.details').on('click', function(e) {
     // Add click event for scroll-to-top button
     scrollToTopButton.onclick = scrollToTop;
 
+    // New functionality for Gallery button
+    $(document).ready(function() {
+        // Other code remains unchanged...
+    
+        // New functionality for Gallery button
+        $('#gallery-btn').on('click', function() {
+            const hotelName = $('#selected-hotel').val();
+            const hotelImages = {
+                "Oasis Hotel": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Inland Hotel": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Watergate Hotel": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Hotel 4": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Hotel 5": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Hotel 6": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Hotel 7": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Hotel 8": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+                "Hotel 9": [
+                    "img/background.png",
+                    "img/watergate.png",
+                    "img/inland.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png",
+                    "img/oaisis.png",
+                    "img/watergate.png",
+                    "img/background.png"
+                ],
+            };
+    
+            // Get the images for the selected hotel
+            const images = hotelImages[hotelName] || [];
+    
+            // Clear previous images in the gallery
+            $('#gallery-images').empty();
+    
+            // Populate the gallery with images
+            images.forEach(image => {
+                $('#gallery-images').append(`
+                    <div class="col-4 mb-2">
+                        <img src="${image}" class="img-fluid" alt="${hotelName} Room" />
+                    </div>
+                `);
+            });
+    
+            // Show the gallery modal
+            $('#galleryModal').modal('show');
+        });
+    
+        // Close button functionality for Gallery modal
+        $('#galleryModal .btn-close, #galleryModal .btn-secondary').on('click', function() {
+            $('#galleryModal').modal('hide');
+        });
+    });
+    
 });
